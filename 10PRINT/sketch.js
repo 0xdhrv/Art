b23f6045b5d98b35ca0c5319b068e9221c650bd5
0xdhrv <intelligentjimmy@gmail.com>
2020-08-31 02:12:57 +0530

    10PRINT Initial

--- 10PRINT/sketch.js
+++ 10PRINT/sketch.js
@@ -1,48 +1,33 @@
-let cx, cy;
-let secondsRadius;
-let minutesRadius;
-let hoursRadius;
-let clockDiameter;
+let x = 0;
+let y = 0;
+let space = 40;
 
 function setup() {
     createCanvas(480, 480);
-    stroke(255);
-
-    let radius = min(width, height) / 2;
-    secondsRadius = radius * 0.80
-    minutesRadius = radius * 0.5;
-    hoursRadius = radius * 0.25;
-    clockDiameter = radius * 1.65;
-
-    cx = width / 2;
-    cy = height / 2;
+    background(0);
 }
 
 function draw() {
-    background(0, 0, 0);
-    noStroke();
-    fill(21, 21, 21);
-    ellipse(cx, cy, clockDiameter, clockDiameter);
-    
-    let s = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
-    let m = map(minute() + norm(second(), 0, 60), 0, 60, 0, TWO_PI) - HALF_PI;
-    let h = map(hour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI * 2) - HALF_PI;
-
     stroke(255);
-    strokeWeight(2);
-    line(cx, cy, cx + cos(s) * secondsRadius, cy + sin(s) * secondsRadius);
-    strokeWeight(2);
-    line(cx, cy, cx + cos(m) * minutesRadius, cy + sin(m) * minutesRadius);
-    strokeWeight(2);
-    line(cx, cy, cx + cos(h) * hoursRadius, cy + sin(h) * hoursRadius);
+    if (random(1) < 0.5) {
+        noStroke();
+        fill(255);
+        rect(x, y, space, space);
+        strokeWeight(2);
+        stroke(128);
+        line(x, y, x + space, y + space);
 
-    strokeWeight(1);
-    beginShape(POINTS);
-    for (let a = 0; a < 360; a += 6) {
-        let angle = radians(a);
-        let x = cx + cos(angle) * secondsRadius;
-        let y = cy + sin(angle) * secondsRadius;
-        vertex(x, y);
+    } else {
+        noStroke();
+        fill(0);
+        rect(x, y, space, space);
+        strokeWeight(2);
+        stroke(128);
+        line(x, y + space, x + space, y);
     }
-    endShape();
+    x = x + space / 4;
+    if (x > width) {
+        x = 0;
+        y = y + space / 4;
+    }
 }
