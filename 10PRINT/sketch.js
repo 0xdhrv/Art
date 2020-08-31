6acd64adb910c9068d60b55c9061ece56026a5fb
0xdhrv <intelligentjimmy@gmail.com>
2020-08-31 09:48:32 +0530

    10PRINT Simple

--- 10PRINT/sketch.js
+++ 10PRINT/sketch.js
@@ -3,31 +3,31 @@ let y = 0;
 let space = 40;
 
 function setup() {
-    createCanvas(480, 480);
+    createCanvas(400, 400);
     background(0);
 }
 
 function draw() {
     stroke(255);
     if (random(1) < 0.5) {
-        noStroke();
-        fill(255);
-        rect(x, y, space, space);
+        // noStroke();
+        // fill(255);
+        // rect(x, y, space, space);
         strokeWeight(2);
         stroke(128);
         line(x, y, x + space, y + space);
 
     } else {
-        noStroke();
-        fill(0);
-        rect(x, y, space, space);
+        // noStroke();
+        // fill(0);
+        // rect(x, y, space, space);
         strokeWeight(2);
         stroke(128);
         line(x, y + space, x + space, y);
     }
-    x = x + space / 4;
+    x = x + space;
     if (x > width) {
         x = 0;
-        y = y + space / 4;
+        y = y + space;
     }
 }
